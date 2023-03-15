fa86a6395a79d7b29d115785c0b5dafd550e07c1
Piotr Duda <piotr.duda00@gmail.com>
2023-03-15 14:08:27 +0100

    bottom navigation

---
+++ screens/AbousUs.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import {Text, View} from 'react-native';
+
+const AboutUs = () => {
+
+    return (
+        <View>
+            <Text>Cart</Text>
+        </View>
+    );
+};
+
+export default AboutUs;
