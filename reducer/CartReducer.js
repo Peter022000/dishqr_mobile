efccdf9df3bfd30fe970ac385d6ca4f037b7ce6a
Piotr Duda <piotr.duda00@gmail.com>
2023-11-27 20:29:01 +0100

    clear

--- reducer/CartReducer.js
+++ reducer/CartReducer.js
@@ -7,14 +7,8 @@ const initialState = {
 
 const CartReducer = (state = initialState, action) => {
     switch (action.type) {
-        case 'ADD_TO_CART':
-            return {
-                dishes: action.payload.data.order,
-                tableNoId: action.payload.data.tableNoId,
-                paymentMethod: action.payload.data.paymentMethod,
-                cost: action.payload.data.cost
-            };
         case 'REMOVE_FROM_CART':
+        case 'ADD_TO_CART':
             return {
                 dishes: action.payload.data.order,
                 tableNoId: action.payload.data.tableNoId,
